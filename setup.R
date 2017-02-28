1023495ee221b55fd3a28155f1fb038f5abca619
Alex Earll <earll9@uw.edu>
2017-02-28 15:58:59 -0800

    Add setup comments and file structure

---
+++ setup.R
@@ -0,0 +1,22 @@
+
+library(shiny)
+library(ggplot2)
+library(dplyr)
+library(plotly)
+library(shiny) 
+
+###Setup###
+# Read in files from the pokemon api, preparing them for use in the rest of the project
+
+
+###part 1###
+#create shiny interface for selecting pokemon
+
+###part 2### 
+#Create backend (sever) for math calculations and such to feed into the UI
+
+###part 3###
+#Create battle sequence 
+
+###part 4### 
+#Create output and summary of the battle
