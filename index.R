faccba288abfbef3a67bceb4f469cc639bbf81e3
Soobin Kwon <soobkwon@uw.edu>
2017-03-03 19:25:11 -0800

    Add list of pokemons

---
+++ index.R
@@ -0,0 +1,26 @@
+# Loads the necessary libraries
+library("httr")
+library("jsonlite")
+library("knitr")
+library("dplyr")
+
+base.uri <- "http://pokeapi.co/"
+generation.1 <- "api/v2/generation/1"
+generation.2 <- "api/v2/generation/2"
+
+uri.generation.1 <- paste0(base.uri, generation.1)
+response.generation.1 <- GET(uri.generation.1)
+body.1 <- fromJSON(content(response.generation.1, "text", encoding = "UTF-8"))
+pokemon1 <- flatten(body.1$pokemon_species)
+
+uri.generation.2 <- paste0(base.uri, generation.2)
+response.generation.2 <- GET(uri.generation.2)
+body.2 <- fromJSON(content(response.generation.2, "text", encoding = "UTF-8"))
+pokemon2 <- flatten(body.2$pokemon_species)
+
+pokemon1.vector <- as.vector(unlist(pokemon1$name))
+pokemon2.vector <- as.vector(unlist(pokemon2$name))
+
+pokemon.names <- c(pokemon1.vector, pokemon2.vector)
+pokemon.names <- as.data.frame(pokemon.names)
+
